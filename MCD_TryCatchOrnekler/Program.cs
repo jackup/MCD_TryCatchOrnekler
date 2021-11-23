a4e418e4459b4b64091a109eacce7eda64fce8a0
jackup <yk.58@hotmail.com>
2021-11-23 18:42:57 +0300

    Add project files.

---
+++ MCD_TryCatchOrnekler/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_TryCatchOrnekler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                int a;
+                Console.Write("Sayı Gir: ");
+                a = int.Parse(Console.ReadLine());
+                Console.WriteLine("Teşekkürler.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata Oluştu!");
+                Console.WriteLine(DateTime.Now);
+                Console.WriteLine("HATA: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Burası Finally");
+            }
+        }
+    }
+}
